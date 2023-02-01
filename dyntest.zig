0cae34dc70dd62c9cfd1e55c4ebf2456d3208b72
Michael Odden <me@michaelodden.com>
2023-02-02 00:47:00 +0100

    dyntest: I think I solved it...

--- dyntest.zig
+++ dyntest.zig
@@ -2,61 +2,69 @@ const std = @import("std");
 const testing = std.testing;
 const print = std.debug.print;
 
-// pub const IntParser = struct {
-//     usingnamespace Parser.Methods(IntParser);
-//     __v: *const Parser.VTable = &vtable,
-
-//     const vtable = Parser.VTable { .parse = parseImpl };
-
-//     fn parseImpl(itself: *const Parser) void {
-//         const self = @ptrCast(*const IntParser, itself);
-//         print("intparse!\n", .{});
-//         _ = self;
-//     }
-// };
-
-pub const Parser = struct {
-    __v: *const VTable,
-    pub usingnamespace Methods(Parser);
-
-    pub fn Methods(comptime T: type) type {
-        return extern struct {
-            pub inline fn parse(self: *const T) void {
-                self.__v.parse();
-            }
-        };
-    }
-
-    pub const VTable = extern struct {
-        parse: *const fn() void,
-    };
-};
-
-
-pub fn createParserType(comptime funcImpl: fn () void) type {
+pub fn ParserForResultType(comptime ResultT: type) type {
     return struct {
-        usingnamespace Parser.Methods(@This());
-        __v: *const Parser.VTable = &vtable,
+        const Self = @This();
+        __v: *const VTable,
+        pub usingnamespace Methods(Self);
 
-        const vtable = Parser.VTable { .parse = funcImpl };
+        pub fn Methods(comptime T: type) type {
+            return extern struct {
+                pub inline fn parse(self: *const T, raw: []const u8, result: *ResultT) void {
+                    self.__v.parse(@ptrCast(*const ParserForResultType(ResultT), self), raw, result);
+                }
+                // pub inline fn fieldName(self: *const T) comptime []const u8 {
+
+                // }
+            };
+        }
+
+        pub const VTable = extern struct {
+            parse: *const fn(self: *const ParserForResultType(ResultT), []const u8, *ResultT) void,
+        };
+
+        pub fn createParserType(comptime dataType: type, comptime field: []const u8, comptime funcImpl: fn ([]const u8) dataType) type {
+            return struct {
+                usingnamespace Self.Methods(@This());
+                __v: *const Self.VTable = &vtable,
+
+                const vtable = Self.VTable {
+                    .parse = actualParse,
+                };
+
+                // Make this the .parse of .VTable
+                pub fn actualParse(iself: *const ParserForResultType(ResultT), raw: []const u8, result: *ResultT) void {
+                    _ = @ptrCast(*const @This(), iself);
+                    @field(result, field) = funcImpl(raw);
+                }
+            };
+        }
     };
 }
 
-fn intParseImpl() void {
+
+const Result = struct { a: usize, b: []const u8 };
+
+fn intParseImpl(raw: []const u8) usize {
+    _ = raw;
+    // _ = result;
     print("intparse!\n", .{});
+    return 321;
 }
 
-fn stringParseImpl() void {
+fn stringParseImpl(raw: []const u8) []const u8 {
+    _ = raw;
     print("stringparse!\n", .{});
+    return "321";
 }
 
 test "2dyn" {
     var allocator = std.testing.allocator;
-    var intparser = createParserType(intParseImpl){};
-    var stringparser = createParserType(stringParseImpl){};
-    // var intparser = try allocator.create(createParserType(intParseImpl));
-    // defer allocator.destroy(intparser);
-    // intparser.* = .{};
+
+    const Parser = ParserForResultType(Result);
+
+    var intparser = Parser.createParserType(usize, "a", intParseImpl){};
+    var stringparser = Parser.createParserType([]const u8, "b", stringParseImpl){};
 
     
     var list = std.ArrayList(*Parser).init(allocator);
@@ -65,9 +73,17 @@ test "2dyn" {
     try list.append(@ptrCast(*Parser, &intparser));
     try list.append(@ptrCast(*Parser, &stringparser));
 
+
+    var result: Result = undefined;
     for (list.items) |entry| {
-        entry.parse();
+        entry.parse("321", &result);
     }
+
+    try testing.expect(result.a == 321);
+    try testing.expectEqualStrings("321", result.b);
 
     // Can I store them in a map, dynamically look up and utilize to populate a struct?
 }
+
+// Plan:
+// If all good: Implement error-handling
